ca4c92521b88a31fc30864ff75d7ff0d1d7a860a
hasparus <hasparus@gmail.com>
2021-12-12 13:03:20 +0100

    Show full host

--- packages/vscode-host/demo/http.js
+++ packages/vscode-host/demo/http.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var serveStatic = require('serve-static')
+var express = require("express");
+var serveStatic = require("serve-static");
 
-var staticBasePath = './demo';
- 
-var app = express()
- 
-app.use(serveStatic(staticBasePath))
-app.listen(8080)
-console.log('Listening on port 8080');
+var staticBasePath = "./demo";
+
+var app = express();
+
+app.use(serveStatic(staticBasePath));
+app.listen(8080);
+console.log("Listening on port http://localhost:8080");
