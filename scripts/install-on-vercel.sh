55204a813c4cdbd671fe9106c3fe07cccc7a8e20
hasparus <hasparus@gmail.com>
2021-12-13 19:21:36 +0100

    Remove sudo from install-on-vercel.sh

--- scripts/install-on-vercel.sh
+++ scripts/install-on-vercel.sh
@@ -3,7 +3,7 @@
 # We need to install xkbfile to build VSCode.
 # `gyp: Call to '${PKG_CONFIG:-pkg-config} x11 xkbfile --libs' returned exit status 1 while in binding.gyp. while trying to load binding.gyp``
 # see https://stackoverflow.com/questions/55878536/no-package-xkbfile-found-when-build-vscode-on-ubuntu
-sudo apt-get install -y g++ gcc make python2.7 pkg-config libx11-dev libxkbfile-dev libsecret-1-dev
+apt-get install -y g++ gcc make python2.7 pkg-config libx11-dev libxkbfile-dev libsecret-1-dev
 
 # Vercel doesn't have PNPM.
 npm i -g pnpm@6.23.6
