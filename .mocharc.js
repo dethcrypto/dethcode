6f6743ccf246254822a377047dd2c5c1c768a48f
hasparus <hasparus@gmail.com>
2022-03-29 17:49:10 +0200

    Use esbuild-register instead of ts-node

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  require: ["ts-node/register/transpile-only"],
+  require: ["esbuild-register"],
   extension: ["ts"],
   watchExtensions: ["ts"],
   // Extension tests run inside of VSCode instance, so we don't include them here
