ee0092b10df417f5eaa121aa0adad2864a47b95b
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-10-22 14:43:28 +0200

    Cleanup, move settings to workbench, move product.json to workbench

--- packages/vscode-host/src/src/vs/code/browser/workbench/workbench.ts
+++ packages/vscode-host/src/src/vs/code/browser/workbench/workbench.ts
@@ -1,8 +1,6 @@
 import { Event } from "vs/base/common/event";
-import { URI, UriComponents } from "vs/base/common/uri";
 import { localize } from "vs/nls";
 import { create } from "vs/workbench/workbench.web.main";
-import { IWorkspace } from "vs/platform/workspace/common/workspace";
 
 import {
   CommandId,
@@ -14,56 +12,22 @@ import { patchForWorkingInIframe } from "../../../deth/in-iframe/patchForWorking
 async function main() {
   patchForWorkingInIframe();
 
-  // create workbench
-  let config: any & {
-    folderUri?: UriComponents;
-    workspaceUri?: UriComponents;
-  } = {};
-
-  if ((window as any).product) {
-    config = (window as any).product;
-  } else {
-    const result = await fetch("/product.json");
-    config = await result.json();
-    // for non https environments (like devmode), we need to tweak protocol to load extensions
-    if (location.protocol !== "https:") {
-      config.additionalBuiltinExtensions.forEach((extension: any) => {
-        extension.scheme = "http";
-      });
-    }
-  }
-
-  if (Array.isArray(config.additionalBuiltinExtensions)) {
-    config.additionalBuiltinExtensions.forEach((extension: any) => {
-      extension.extensionLocation = URI.revive(extension.extensionLocation);
-    });
-  }
-
-  let workspace = undefined;
-  if (config.folderUri) {
-    workspace = { folderUri: URI.revive(config.folderUri) };
-  } else if (config.workspaceUri) {
-    workspace = { workspaceUri: URI.revive(config.workspaceUri) };
-  }
-
-  if (workspace) {
-    const workspaceProvider: any = {
-      workspace,
-      open: async (
-        _workspace: IWorkspace,
-        _options?: { reuse?: boolean; payload?: object }
-      ) => true,
-      trusted: true,
-    };
-    config = { ...config, workspaceProvider };
-  }
+  const config = getProductInfo();
 
   const apiName = ethViewerCommands.getApiName() || "etherscan";
 
+  console.log(config);
+
   create(document.body, {
     ...config,
+    settingsSyncOptions: {
+      enabled: false,
+    },
     commands: getCommands(),
     configurationDefaults: {
+      "window.commandCenter": false,
+      "workbench.colorTheme": "Default Dark Modern",
+      "telemetry.enableTelemetry": false,
       // Omits ${rootName} "Untitled (Workspace)" from the title
       "window.title":
         "${dirty}${activeEditorShort}${separator}${appName}${separator}${remoteName}",
@@ -77,4 +41,78 @@ async function main() {
   });
 }
 
-main().catch(console.error);
+
+type Mutable<T> = {
+  -readonly [K in keyof T]: Mutable<T[K]>;
+}
+type ProductInfoType = Mutable<ReturnType<typeof getProductionProductInfo>>;
+
+function getProductInfo(): ProductInfoType {
+  if (location.protocol === "https:") {
+    return getProductionProductInfo() as any
+  } else {
+    return getDevelopmentProductInfo() as any
+  }
+}
+
+function getProductionProductInfo() {
+  return {
+    "productConfiguration": {
+      "nameShort": "DethCode",
+      "nameLong": "DethCode",
+      "applicationName": "deth-viewer",
+      "dataFolderName": ".deth-viewer",
+      "version": "1.82.0",
+      "extensionEnabledApiProposals": {
+        "deth.ethereum-viewer": ["fileSearchProvider", "textSearchProvider"]
+      }
+    },
+    "additionalBuiltinExtensions": [
+      {
+        "scheme": "https",
+        "path": "/extensions/solidity-extension"
+      },
+      {
+        "scheme": "https",
+        "path": "/extensions/vyper-syntax"
+      },
+      {
+        "scheme": "https",
+        "path": "/extensions/ethereum-viewer"
+      }
+    ]
+  } as const
+}
+
+function getDevelopmentProductInfo() {
+  return {
+    "productConfiguration": {
+      "nameShort": "DethCode",
+      "nameLong": "DethCode",
+      "applicationName": "deth-viewer",
+      "dataFolderName": ".deth-viewer",
+      "version": "1.82.0",
+      "extensionEnabledApiProposals": {
+        "deth.ethereum-viewer": ["fileSearchProvider", "textSearchProvider"]
+      }
+    },
+    "additionalBuiltinExtensions": [
+      {
+        "scheme": "http",
+        "path": "/static/extensions/0/solidity-extension"
+      },
+      {
+        "scheme": "http",
+        "path": "/static/extensions/0/vyper-syntax"
+      },
+      {
+        "scheme": "http",
+        "path": "/static/extensions/0/ethereum-viewer"
+      }
+    ]
+  } as const
+}
+
+main().catch((e) => {
+  console.error("Error while workbench initialization", e)
+});
